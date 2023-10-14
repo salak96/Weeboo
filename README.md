dc23a5f9c830f98a86020f4099fb7511d220b2db
salak96 <lambangsasangka@gmail.com>
2023-10-14 18:54:25 +0700

    weeboo

---
+++ README.md
@@ -0,0 +1,16 @@
+# WEEBOO ANIME React + Vite
+- Install pnpm
+    - ```bash
+    $ npm install
+    ```
+- Run   
+    - ```bash
+    npm run dev
+    ```
+
+# Contributing
+- Sandikha Galih
+- [Search] Add new features
+- [Search] Fix bugs
+
+
---
+++ src/components/Main/Anime.jsx
@@ -0,0 +1,15 @@
+export default function Anime({ anime, onSelectedAnime }) {
+    return (
+        <>
+            <li onClick={() => onSelectedAnime(anime.mal_id)}>
+                <img src={anime.image} alt={`${anime.title} cover`} />
+                <h3>{anime.title}</h3>
+                <div>
+                    <p>
+                        <span>{anime.year}</span>
+                    </p>
+                </div>
+            </li>
+        </>
+    );
+}
